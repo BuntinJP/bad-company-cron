c0bff71931e7f1a3ee5b1e108acfb991d97cb39e
Buntin-Synthia <aokitakumi72@gmail.com>
2023-05-16 00:58:37 +0900

    Hello, Cloudflare Workers KV

---
+++ README.md
@@ -0,0 +1,7 @@
+# bad-company-cron
+
+## Description
+
+manga-eater-discord-client と、manga-eater サーバーを繋ぐ Cloudflare Workers のプログラムです。
+
+cron のみで使用することを想定しています。
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,26 @@
 export default {
-  async fetch(request, env) {
-    return new Response("Hello world")
-  }
-}
+  async fetch(request: Request, env: any) {
+    return new Response('Hello world');
+  },
+  async scheduled(event: ScheduledEvent, env: any, ctx: any) {
+    console.log(event.scheduledTime);
+    await initKV(event, env);
+  },
+};
+
+const initKV = async (event: ScheduledEvent, env: any) => {
+  // 時間を記録
+  const me: KVNamespace = env.ME;
+  const date = new Date().toISOString();
+  await me.put('lastExecution', date);
+  console.log(await me.get('test'));
+};
+
+/* async function handleScheduled(event: ScheduledEvent) {
+  let lastExecution = await MY_KV_NAMESPACE.get('lastExecution');
+
+  // 仕事をする
+  // 例: APIをフェッチする、データベースを更新する、メールを送信するなど
+
+  await MY_KV_NAMESPACE.put('lastExecution', new Date().toISOString());
+} */
